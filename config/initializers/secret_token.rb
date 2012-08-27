6ff4396ee4aa0a640c53ee2b016478d9cb493d09
Jeremy Fox <atljeremy@me.com>
2012-08-27 08:37:22 -0400

    Created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atlmetal::Application.config.secret_token = '4cae0df7318df46eda919d692048348304461dfc5d49eaad9e49359bc228fbfa42b5b8ab255bf5ad197fe043c266df83c693f33f677473375eecf2d4b75501dc'
